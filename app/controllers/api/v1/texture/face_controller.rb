0a7542abf43ee39b13b7a04b2c112a35fa6cb53c
myosotis-dev <github@myosotis.dev>
2023-08-26 00:56:06 +0900

    add Rails files.

---
+++ app/controllers/api/v1/texture/face_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::Texture::FaceController < ApplicationController
+	def show
+		@face = Mineface::Face.new name: params[:id]
+		begin
+			@face.request!
+			@image_bin = @face.image.to_blob do
+				self.format = "PNG"
+			end
+		rescue => e
+			warn "[WARNING]: #{e.message}"
+			@image_bin = steve_face_image.to_blob do
+				self.format = "PNG"
+			end
+		end
+		send_data @image_bin, type: "image/png", disposition: 'inline'
+	end
+
+	private def steve_face_image
+		@face = Mineface::Face.new
+		return @face.get_face_image(Rails.root.join('public', "steve.png").to_s)
+	end
+end
