2cfefe82761624f9bf412dc1f0ac6ad5224ce46a
myosotis-dev <github@myosotis.dev>
2023-09-17 21:39:16 +0900

    update spec

--- spec/requests/api/v1/servers/status_spec.rb
+++ spec/requests/api/v1/servers/status_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Servers::Statuses", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/servers/status/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+	describe "index action" do
+		# it "returns http success" do
+		# 	get api_v1_servers_status_path params: {host: "minecraft.example.com"}
+		# 	expect(response).to have_http_status(200)
+		# end
+	end
 end
