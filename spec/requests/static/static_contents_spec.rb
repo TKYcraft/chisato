58ec48f45f29bc72befa40d5c21cf86e4cd0f664
myosotis-dev <github@myosotis.dev>
2023-10-01 20:06:22 +0900

    add request spec for checking delivering static files.

--- spec/requests/static/static_contents_spec.rb
+++ spec/requests/static/static_contents_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe "GET static contents", type: :request do
 			expect(response).to have_http_status 200
 		end		
 	end
+
+	describe "./public/index.html" do
+		it "returns status success with /" do
+			get "/"
+			expect(response).to have_http_status 200
+		end
+
+		it "returns status success with /index.html" do
+			get "/index.html"
+			expect(response).to have_http_status 200
+		end
+	end
+
+	describe "./public/robots.txt" do
+		it "returns status success" do
+			get "/robots.txt"
+			expect(response).to have_http_status 200
+		end
+	end
 end
