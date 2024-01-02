1ae268bd2cfa7d0acfadcb8de87e89e4697a5398
myosotis-dev <github@myosotis.dev>
2024-01-03 00:25:02 +0900

    replace Minetools::FaceTool::Face to described_class on Rspec

--- spec/lib/minetools/face_tool/face_spec.rb
+++ spec/lib/minetools/face_tool/face_spec.rb
@@ -3,8 +3,8 @@ require './lib/minetools/face_tool/face.rb'
 
 RSpec.describe Minetools::FaceTool::Face do
 	it "is object of Minetools::FaceTool::Face class" do
-		face = Minetools::FaceTool::Face.new
-		expect(face.class).to eq(Minetools::FaceTool::Face)
+		face = described_class.new
+		expect(face.class).to eq(described_class)
 	end
 
 	describe "methods" do
@@ -14,7 +14,7 @@ RSpec.describe Minetools::FaceTool::Face do
 					_n = "KrisJelbring"   # KrisJelbring is developer of minecraft
 					_s = 1024
 
-					face = Minetools::FaceTool::Face.new name: _n, size: _s
+					face = described_class.new name: _n, size: _s
 
 					expect(face.name).to eq _n
 					expect(face.size).to eq _s
@@ -25,7 +25,7 @@ RSpec.describe Minetools::FaceTool::Face do
 		describe "get_minecraft_uuid()" do
 			context "give correct name" do
 				it "returns currect uuid" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					uuid = face.get_minecraft_uuid 'KrisJelbring'
 					expect(uuid).to eq "7125ba8b1c864508b92bb5c042ccfe2b"
 				end
@@ -33,7 +33,7 @@ RSpec.describe Minetools::FaceTool::Face do
 
 			context "give none-existent user name" do
 				it "raise GetUUIDError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					# Can't get '!!!' as user name of minecraft.
 					expect{face.get_minecraft_uuid '!!!'}.to raise_error(Minetools::FaceTool::GetUUIDError)
 				end
@@ -41,28 +41,28 @@ RSpec.describe Minetools::FaceTool::Face do
 
 			context "give nothing" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_uuid}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give argument which is not String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_uuid -1}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give empty String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_uuid ""}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give nil" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_uuid nil}.to raise_error(ArgumentError)
 				end
 			end
@@ -71,7 +71,7 @@ RSpec.describe Minetools::FaceTool::Face do
 		describe "request_json()" do
 			context "give correct url" do
 				it "returns hash object" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					# TODO : change mock api...
 					json = face.request_json "https://api.github.com/status"
 					expect(json.class).to eq Hash
@@ -80,42 +80,42 @@ RSpec.describe Minetools::FaceTool::Face do
 
 			context "give nothing" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request_json}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give argument which is not String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request_json -1}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give empty String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request_json ""}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give nil" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request_json nil}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give url which is non-exists site" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request_json "https://example.example.com/"}.to raise_error(Minetools::FaceTool::APIRequestError)
 				end
 			end
 
 			context "give url which is not able to parse json" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request_json "https://example.com/"}.to raise_error(Minetools::FaceTool::APIRequestError)
 				end
 			end
@@ -124,7 +124,7 @@ RSpec.describe Minetools::FaceTool::Face do
 		describe "get_minecraft_profile()" do
 			context "give correct uuid" do
 				it "returns correct profile" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					# This is uuid of KrisJelbring, he is developer of minecraft.
 					uuid = "7125ba8b1c864508b92bb5c042ccfe2b"
 					profile = face.get_minecraft_profile uuid
@@ -149,35 +149,35 @@ RSpec.describe Minetools::FaceTool::Face do
 
 			context "give none-existent uuid" do
 				it "raise GetProfileError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile "foo"}.to raise_error(Minetools::FaceTool::GetProfileError)
 				end
 			end
 
 			context "give nothing" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give argument which is not String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile -1}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give empty String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile ""}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give nil" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile}.to raise_error(ArgumentError)
 				end
 			end
@@ -186,7 +186,7 @@ RSpec.describe Minetools::FaceTool::Face do
 		describe "get_skin_image_url()" do
 			context "give correct uuid" do
 				it "returns correct url" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					# This is uuid of KrisJelbring, he is developer of minecraft.
 					uuid = "7125ba8b1c864508b92bb5c042ccfe2b"
 					image_url = face.get_skin_image_url uuid
@@ -196,35 +196,35 @@ RSpec.describe Minetools::FaceTool::Face do
 
 			context "give none-existent uuid" do
 				it "raise GetProfileError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile "foo"}.to raise_error(Minetools::FaceTool::GetProfileError)
 				end
 			end
 
 			context "give nothing" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give argument which is not String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile -1}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give empty String" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile ""}.to raise_error(ArgumentError)
 				end
 			end
 
 			context "give nil" do
 				it "raise ArgumentError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.get_minecraft_profile}.to raise_error(ArgumentError)
 				end
 			end
@@ -236,7 +236,7 @@ RSpec.describe Minetools::FaceTool::Face do
 			context "give correct arguments" do
 				it "returns image" do
 					url = "http://textures.minecraft.net/texture/b47b21bb3e7f79bdf2a5e8e041f7ff9e178dc15645f6449b8e55f906604c07f9"
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect(face.get_face_image(url).class).to eq Magick::Image
 				end
 			end
@@ -244,28 +244,28 @@ RSpec.describe Minetools::FaceTool::Face do
 			context "give bad value to skin_image_url" do
 				context "give nothing" do
 					it "raise ArgumentError" do
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image}.to raise_error ArgumentError
 					end
 				end
 
 				context "give value which is not String" do
 					it "raise ArgumentError" do
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image -1}.to raise_error ArgumentError
 					end
 				end
 
 				context "give empty String" do
 					it "raise ArgumentError" do
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image ""}.to raise_error ArgumentError
 					end
 				end
 
 				context "give nil" do
 					it "raise ArgumentError" do
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image nil}.to raise_error ArgumentError
 					end
 				end
@@ -275,7 +275,7 @@ RSpec.describe Minetools::FaceTool::Face do
 				context "give value which is not Integer" do
 					it "raise ArgumentError" do
 						url = "http://textures.minecraft.net/texture/b47b21bb3e7f79bdf2a5e8e041f7ff9e178dc15645f6449b8e55f906604c07f9"
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image url, "string"}.to raise_error ArgumentError
 					end
 				end
@@ -283,7 +283,7 @@ RSpec.describe Minetools::FaceTool::Face do
 				context "give value which is not multiple of 8" do
 					it "raise ArgumentError" do
 						url = "http://textures.minecraft.net/texture/b47b21bb3e7f79bdf2a5e8e041f7ff9e178dc15645f6449b8e55f906604c07f9"
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image url, 11}.to raise_error ArgumentError
 					end
 				end
@@ -291,7 +291,7 @@ RSpec.describe Minetools::FaceTool::Face do
 				context "give minus value" do
 					it "raise ArgumentError" do
 						url = "http://textures.minecraft.net/texture/b47b21bb3e7f79bdf2a5e8e041f7ff9e178dc15645f6449b8e55f906604c07f9"
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image url, -1}.to raise_error ArgumentError
 					end
 				end
@@ -299,7 +299,7 @@ RSpec.describe Minetools::FaceTool::Face do
 				context "give nil" do
 					it "raise ArgumentError" do
 						url = "http://textures.minecraft.net/texture/b47b21bb3e7f79bdf2a5e8e041f7ff9e178dc15645f6449b8e55f906604c07f9"
-						face = Minetools::FaceTool::Face.new
+						face = described_class.new
 						expect{face.get_face_image url, nil}.to raise_error ArgumentError
 					end
 				end
@@ -309,7 +309,7 @@ RSpec.describe Minetools::FaceTool::Face do
 		describe "request!()" do
 			context "set name to instance correctly" do
 				it "set some instance variables" do
-					face = Minetools::FaceTool::Face.new name: "KrisJelbring"
+					face = described_class.new name: "KrisJelbring"
 					face.request!
 					expect(face.uuid).to be_present
 					expect(face.skin_image_url).to be_present
@@ -317,19 +317,19 @@ RSpec.describe Minetools::FaceTool::Face do
 				end
 
 				it "returns nil" do
-					face = Minetools::FaceTool::Face.new name: "KrisJelbring"
+					face = described_class.new name: "KrisJelbring"
 					expect(face.request!).to eq nil
 				end
 
 				it "not raise errors" do
-					face = Minetools::FaceTool::Face.new name: "KrisJelbring"
+					face = described_class.new name: "KrisJelbring"
 					expect{face.request!}.not_to raise_error
 				end
 			end
 
 			context "not set name to instance" do
 				it "raise FaceRequestError" do
-					face = Minetools::FaceTool::Face.new
+					face = described_class.new
 					expect{face.request!}.to raise_error Minetools::FaceTool::FaceRequestError
 				end
 			end
@@ -338,7 +338,7 @@ RSpec.describe Minetools::FaceTool::Face do
 
 	describe "instance variables" do
 		it "default values" do
-			face = Minetools::FaceTool::Face.new
+			face = described_class.new
 			expect(face.name).to eq nil
 			expect(face.uuid).to eq nil
 			expect(face.skin_image_url).to eq nil
